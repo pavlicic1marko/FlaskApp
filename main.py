cd17013bf710271cac5161ef88fc991798a98b64
Marko Pavlicic <marko.pavlicic@computerrock.com>
2023-12-12 15:53:02 +0100

    adding GET and POST methods for /cars route

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from markupsafe import escape
+import random
 
 
 app = Flask(__name__, template_folder='templates')
 
 users = {"users": [{"name": "marko", "age": 12}, {"name": "wewe", "age": 132}]}
+car_list =[
+    {
+    "id":0,
+    "model":"BMW-RT",
+    "price":"10"
+     },
 
+    {
+    "id":1,
+    "model":"BMW-XC",
+    "price":"20"
+    }
+]
 
 @app.route("/")
 def index():
@@ -14,7 +27,7 @@ def index():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
+    #note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
 
@@ -29,13 +42,39 @@ def hello():
 
 @app.route('/test')
 def test():
-    return jsonify({"users": "test"})
+    return jsonify({"users": "testss"})
 
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return f'User {escape(username)}'
 
+@app.route('/cars', methods=['GET','POST'])
+def cars():
+    if request.method == 'GET':
+        if len(car_list)>0:
+            return jsonify(car_list)
+        else:
+            return 'no cars found', 404
+
+    if request.method == 'POST':
+        new_id = random.randint(1000, 2000)
+        new_model = request.form['model']
+        new_price = request.form['price']
+
+        new_obj = {
+            'id': new_id,
+            'model': new_model,
+            'price': new_price
+        }
+        car_list.append(new_obj)
+        return jsonify(car_list),201
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=80)
