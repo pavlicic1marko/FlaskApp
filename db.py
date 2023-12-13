f964da8b4f32809821440f3863b7ed147ce259d0
Marko Pavlicic <marko.pavlicic@computerrock.com>
2023-12-13 12:36:50 +0100

    adding db.py for sqlite cars table, adding to .gitignore

--- db.py
+++ db.py
@@ -1,3 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect("cars.sqlite")
+conn = sqlite3.connect("cars.sqlite") # crete if not exist
+
+cursor = conn.cursor()
+sql_query = """CREATE TABLE cars  (id integer PRIMARY KEY,
+        model text NOT NULL,
+        price integer NOT NULL
+)"""
+cursor.execute(sql_query)
