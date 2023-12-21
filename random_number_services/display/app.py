1c647cdf6bd437c1d1c7e18b7bbed0c28d91816b
Marko Pavlicic <marko.pavlicic@computerrock.com>
2023-12-21 18:07:55 +0100

    adding network error handling for microservice

--- random_number_services/display/app.py
+++ random_number_services/display/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import requests
+from urllib3.exceptions import NewConnectionError
 
 app = Flask(__name__)
 
@@ -12,9 +13,14 @@ def call_random_microservie():
     return response.json().get("random_number")
 
 
+
 @app.route("/", methods=['GET'])
 def check_even_odd():
-    random_number = call_random_microservie()
+    try:
+        random_number = call_random_microservie()
+    except requests.exceptions.ConnectionError  as e:
+        return jsonify({"random_number": 'there is an  connection error'})
+
     result = "even" if random_number % 2 == 0 else "odd"
     return jsonify({"random_number": random_number, "result": result})
 
