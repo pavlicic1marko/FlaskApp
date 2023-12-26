f9ab20f9e4ad3fac07f65a3b25003691df578a10
Marko Pavlicic <marko.pavlicic@computerrock.com>
2023-12-26 10:55:07 +0100

    adding layout.html and about.html

--- frontend.py
+++ frontend.py
@@ -4,11 +4,18 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 random_microservice_url = "http://127.0.0.1:5000"
 
+
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
 def main_page():
     user_data = get_user_data()
-    return render_template('test.html', user_data=user_data)
+    return render_template('test.html', user_data=user_data, title='BEST')
+
+
+@app.route('/about', methods=['GET'])
+def about_page():
+    user_data = get_user_data()
+    return render_template('about.html', user_data=user_data, title='BEST')
 
 
 def get_user_data():
