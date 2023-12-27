7ed7873d81ccdb68f91c2ad98c5a729570908414
Marko Pavlicic <marko.pavlicic@computerrock.com>
2023-12-27 17:03:48 +0100

    implementing flash messages

--- forms.py
+++ forms.py
@@ -5,9 +5,9 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo(password)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('register')
 
 
--- frontend.py
+++ frontend.py
@@ -1,8 +1,8 @@
 import requests
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 
 app = Flask(__name__, template_folder='templates')
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm  # ,  LoginForm
 
 # protect against cookie modification, csrf tokens
 app.config['SECRET_KEY'] = 'v34erjlb8o37444rrrr934gfriyf3'
@@ -12,7 +12,7 @@ random_microservice_url = "http://127.0.0.1:5000"
 
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
-def main_page():
+def home():
     user_data = get_user_data()
     return render_template('test.html', user_data=user_data, title='BEST')
 
@@ -23,17 +23,20 @@ def about_page():
     return render_template('about.html', user_data=user_data, title='BEST')
 
 
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', titl='Register', form=form)
 
 
 @app.route('/login')
 def login():
     pass
-    #form = RegistrationForm()
-    #return render_template('login.html', titl='Login', form=form)
+    # form = RegistrationForm()
+    # return render_template('login.html', titl='Login', form=form)
 
 
 def get_user_data():
