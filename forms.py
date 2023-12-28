8a5acf29b9f1328c4780dbaf3043a422f168a56f
Marko Pavlicic <marko.pavlicic@computerrock.com>
2023-12-28 13:45:32 +0100

    implementing flash messages for errors, adding validation

--- forms.py
+++ forms.py
@@ -1,13 +1,12 @@
 from flask_wtf import FlaskForm
 from  wtforms import  PasswordField, BooleanField, SubmitField, StringField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(6, 20)])
     submit = SubmitField('register')
 
 
