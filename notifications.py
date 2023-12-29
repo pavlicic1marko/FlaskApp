5c4a0ce02040c25d2f33b176f2fd9875382bc7b0
Marko Pavlicic <marko.pavlicic@computerrock.com>
2023-12-29 20:40:41 +0100

    importing sql alchemy ORM and creating notifications model

--- notifications.py
+++ notifications.py
@@ -1,17 +1,34 @@
+import datetime
 from flask import Flask, request, make_response, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# database location ///- this folder
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# create database instance
+db = SQLAlchemy(app)
 
-users = [{"name": "marko", "age": "33", "id": "123153765"},{"name": "marko", "age": "33", "id": "123153765"}]
+
+class Notifications(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.UTC )
+
+    def __repr__(self):
+        return f"User('{self.title}','{self.text}')"
+
+
+users = [{"name": "marko", "age": "33", "id": "123153765"}, {"name": "marko", "age": "33", "id": "123153765"}]
 news = [{"title": "GDP jumps 10 times", "text": "GDP of astocka hase increased 10 times"}]
-
 
 
 @app.route("/", methods=['GET'])
 def generate_user():
     return users
 
-@app.route("/api/notifications", methods=['GET','POST'])
+
+@app.route("/api/notifications", methods=['GET', 'POST'])
 def users_change():
     if request.method == 'POST':
         title = request.form['title']
